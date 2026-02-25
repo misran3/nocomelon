cc8e1269d53db21019a55c9b82413e9a2d17324a
Mohammed Misran <mohammed.misran38@gmail.com>
2026-02-25 10:06:06 -0500

    feat(infra): add aws_profile variable for AWS CLI configuration

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -4,6 +4,12 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+variable "aws_profile" {
+  description = "AWS CLI profile to use"
+  type        = string
+  default     = "default"
+}
+
 variable "app_name" {
   description = "Application name (used for resource naming)"
   type        = string
