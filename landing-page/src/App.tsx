c83df3ad294f4e0fbdd012cae365021197fea8ef
Mohammed Misran <mohammed.misran38@gmail.com>
2026-02-25 20:13:15 -0500

    feat(landing): add why nocomelon section

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import HowItWorks from './components/HowItWorks';
+import WhyNoComelon from './components/WhyNoComelon';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <main>
         <Hero />
         <HowItWorks />
+        <WhyNoComelon />
       </main>
     </div>
   );
